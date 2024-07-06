f17a6b1d99ac385cf77235a1c8e59cd26daf48a6
sydorovay <sydorovay@gmail.com>
2024-07-06 13:04:04 +0200

    finish

--- src/componens/Contact/Contact.jsx
+++ src/componens/Contact/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = ({contact, deleteContact }) => {
   return (
     <div className={css.contactItem}>
       <div className={css.contactInfo}>
-        <span className={css.contactName}><FaUser className={css.icon} />  {contact.name}</span>
-        <span className={css.contactNumber}><FaPhone className={css.icon} />{contact.number}</span>
+        <span className={css.contactName}><span className={css.user} ><FaUser /> </span> {contact.name}</span>
+        <span className={css.contactNumber}><span  className={css.phone}><FaPhone/></span>{contact.number}</span>
       </div>
       <button className={css.deleteButton} onClick={() => deleteContact(contact.id)}>Delete</button>
     </div>
